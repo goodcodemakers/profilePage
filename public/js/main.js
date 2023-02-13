f9e78a2b8f30f8b62ead0950f46469ac10c63424
choi jeong ho <113665732+goodcodemakers@users.noreply.github.com>
2023-02-13 15:28:54 +0900

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// let oddugiImage = [
+//   "./image/oddugimain_1.jpg",
+//   "./image/oddugimain_2.jpg",
+//   "./image/oddugimain_3.jpg",
+// ];
+
+// let oddugi = document.querySelector(".oddugi");
+
+// for (let i = 0; i < oddugiImage.length; i++) {
+//   oddugi.style.background = url(`${oddugiImage[i]}`);
+// }
