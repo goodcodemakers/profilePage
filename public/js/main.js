85350cc9a905cac1bba350ab82c52d1eb1a58f38
choi jeong ho <113665732+goodcodemakers@users.noreply.github.com>
2023-02-13 17:54:52 +0900

    upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,18 @@
-// let oddugiImage = [
-//   "./image/oddugimain_1.jpg",
-//   "./image/oddugimain_2.jpg",
-//   "./image/oddugimain_3.jpg",
-// ];
+let oddugiImage = [
+  "../public/image/oddugimain_1.png",
+  "../public/image/oddugimain_2.png",
+  "../public/image/oddugimain_3.png",
+];
 
-// let oddugi = document.querySelector(".oddugi");
+let oddugi = document.querySelector(".oddugi");
 
-// for (let i = 0; i < oddugiImage.length; i++) {
-//   oddugi.style.background = url(`${oddugiImage[i]}`);
-// }
+let num = 0;
+function sliden() {
+  num++;
+  if (num == 3) {
+    num = 0;
+  }
+  oddugi.style.background = `url(${oddugiImage[num]})`;
+}
+setInterval(sliden, 3000);
+console.log(oddugiImage[2]);
